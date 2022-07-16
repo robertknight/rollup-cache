440a1ad6c95570ee32b183c851d4dc32ee5e9949
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2022-07-16 20:40:50 +0530

    Reuse the commonjs filename
    In the rollup configurations, it feels like it is a good practice to
    reuse the output file name from the package.json.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
+import pkg from "./package.json";
+
+const OUTPUT_COMMON_JS_FILE = pkg.exports.require;
+
 /**
  * @type {import("rollup").RollupOptions}
  */
 export default {
 	input: "index.js",
 	output: {
-		file: "index.cjs",
+		file: OUTPUT_COMMON_JS_FILE,
 		format: "cjs",
 		exports: "auto",
 	},
