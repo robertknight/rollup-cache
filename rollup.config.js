6dc03f29699a3997d55d496d736aa91d15638791
Robert Knight <robertknight@gmail.com>
2022-07-19 20:21:41 +0100

    Fix warnings about missing external dependencies
    Fix warnings about missing imports for the Node standard library and
    Rollup modules. These are expected to exist in the consuming project.
    
    rollup.config.js was also reformatted with Prettier in the process.

--- rollup.config.js
+++ rollup.config.js
@@ -6,10 +6,22 @@ const OUTPUT_COMMON_JS_FILE = pkg.exports.require;
  * @type {import("rollup").RollupOptions}
  */
 export default {
-	input: "index.js",
-	output: {
-		file: OUTPUT_COMMON_JS_FILE,
-		format: "cjs",
-		exports: "auto",
-	},
+  input: "index.js",
+  output: {
+    file: OUTPUT_COMMON_JS_FILE,
+    format: "cjs",
+    exports: "auto",
+  },
+  external: [
+    // Node standard library
+    "crypto",
+    "fs",
+    "fs/promises",
+    "module",
+    "path",
+
+    // Rollup and plugins
+    "rollup",
+    /@rollup\/.*/,
+  ],
 };
